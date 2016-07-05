770569cdec44f5576a1a78386b07ff96ba70eb51
grimyoung <samhyukim@gmail.com>
2016-07-04 19:14:42 -0700

    solved book_titles and timer

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def title=(str)
+		list = str.split(" ")
+		#small_words is not exhaustive
+		small_words = %w(the a an and for but or yet so in at on)
+		list.each_with_index do |word, index|
+			if (word.length >2 and !small_words.include? word) or word == "i" or index == 0
+				word.capitalize!
+			end
+		end
+		@title = list.join(" ")
+	end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def padded(time)
+    if time >10
+      return time.to_s
+    else
+      return "0"+time.to_s
+    end
+  end
+  
+  def time_string
+  	hours = @seconds/3600
+  	minutes = (@seconds%3600)/60
+  	seconds = @seconds-(hours*3600)- (minutes*60)
+    array = [padded(hours),padded(minutes),padded(seconds)]
+    @time_string = array.join(":")
+  end
+
 end
